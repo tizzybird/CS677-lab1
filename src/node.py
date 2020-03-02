0a4705e7edccf7c5a03e0ad8eecdad444593b255
tizzybird <yschen@livemail.tw>
2020-03-01 23:44:19 -0500

    [Feature] Implement raw versions of lookup, reply & buy (TODO: synchronization, request regeneration)

--- src/node.py
+++ src/node.py
@@ -1,7 +1,7 @@
 import xmlrpc
 from xmlrpc.server import SimpleXMLRPCServer
 
-import json
+# import json
 import socket
 import threading as thd
 from threading import Lock
@@ -13,15 +13,19 @@ BUYER  = 1
 SELLER = 2
 BOTH   = 3
 
-PEER_NUM = 6
+FISH = 11
+SALT = 12
+BOAR = 13
+MAX_ITEM_NUMBER = 20
 
-ADDR = socket.gethostbyname(socket.gethostname())
+# ADDR = socket.gethostbyname(socket.gethostname())
 PORT_START_NUM = 10070
 
-MAX_BUYER_NUM  = 2
-MAX_SELLER_NUM = 2
+PEER_NUM = 6
+MAX_BUYER_NUM  = int(PEER_NUM/2) + 1
+MAX_SELLER_NUM = int(PEER_NUM/2) + 1
 
-print_lock = Lock()
+LOCK_PRINT = Lock()
 
 class Peer(thd.Thread):
     def __init__(self, peer_id, role):
@@ -38,51 +42,137 @@ class Peer(thd.Thread):
         
         if len(self.neighbors) == 0:
             self.neighbors.append(peer_id - 1 if peer_id > 0 else peer_id + 1)
-
+            
 
     def run(self):
-        server = thd.Thread(target=self.start_peer_server)
+        server = thd.Thread(target=self.initiate_rpc_server)
         server.start()
 
-        if self.role == BUYER or self.role == BOTH:
-            for i in self.neighbors:
-                addr = "http://localhost:%d/" % (PORT_START_NUM+i)
-                proxy = xmlrpc.client.ServerProxy(addr)
-                with print_lock:
-                    print ("-------------> Peer %d calls neighbor %d" % (self.peer_id, i))
+        # if self.role == BUYER or self.role == BOTH:
+        #     for i in self.neighbors:
+        #         addr = "http://localhost:%d/" % (PORT_START_NUM+i)
+        #         proxy = xmlrpc.client.ServerProxy(addr)
+        #         with LOCK_PRINT:
+        #             print ("-------------> Peer %d calls neighbor %d" % (self.peer_id, i))
                 
-                # testing
-                if proxy.show_me_some_message(self.peer_id):
-                    with print_lock:
-                        print ("-------------- Peer %d finishes calling" % (self.peer_id))
+        #         # testing
+        #         if proxy.show_me_some_message(self.peer_id):
+        #             with LOCK_PRINT:
+        #                 print ("-------------- Peer %d finishes calling" % (self.peer_id))
         
         server.join()
 
+
+    def initiate_buyer(self):
+        # generate a buy request
+        self.target = random.randint(FISH, BOAR)
+        if self.role == BOTH:
+            while self.target == self.commodity:
+                self.target = random.randint(FISH, BOAR)
+        
+        self.target_quantity = random.randint(1, MAX_ITEM_NUMBER)
+        self.timeout = random.randint(3, 9)
+
+        for neighbor_id in self.neighbors:
+            addr = "http://localhost:%d/" % (PORT_START_NUM+neighbor_id)
+            proxy = xmlrpc.client.ServerProxy(addr)
+            proxy.lookup(self.target, self.timeout, '%d' % self.peer_id)
+
+
+    def initiate_seller(self):
+        self.commodity = random.randint(FISH, BOAR)
+        self.commodity_number = random.randint(1, MAX_ITEM_NUMBER)
+
     
     # for testing
-    def show_me_some_message(self, sMsg):
-        with print_lock:
-            print ("<------------- Peer %d receives a call from %s" % (self.peer_id, sMsg))
+    # def show_test_message(self, from_id):
+    #     with LOCK_PRINT:
+    #         print ("<------------- Peer %d receives a call from %s" % (self.peer_id, from_id))
+
+    #     return True
+
+
+    def initiate_rpc_server(self):
+        with LOCK_PRINT:
+            print("The server of peer %d is running" % self.peer_id)
+        server = SimpleXMLRPCServer(("localhost", PORT_START_NUM + self.peer_id))
+        server.register_function(self.lookup)
+        server.register_function(self.reply)
+
+        if self.role != SELLER:
+            server.register_function(self.buy)
+        # server.register_function(self.show_test_message)
+        server.serve_forever()
+
+
+    def lookup(self, product_name, hopcount, path):
+        footprints = path.split('-')
+        if self.role == BUYER:
+            # discard
+            if hopcount == 0:
+                return False
+
+            for neighbor_id in self.neighbors:
+                if str(neighbor_id) not in footprints:
+                    addr = "http://localhost:%d/" % (PORT_START_NUM + neighbor_id)
+                    proxy = xmlrpc.client.ServerProxy(addr)
+                    proxy.lookup(product_name, hopcount-1, "%d-%s" % (self.peer_id, path))
+
+            return True
+
+
+        if product_name == self.commodity:
+            # lock here in case that a traction proceeds while a message saying that
+            # the seller has the item is on the road
+            from_neighbor_id = int(footprints[0])
+            new_path = "-".join(footprints[1:])
+            
+            addr = "http://localhost:%d/" % (PORT_START_NUM + from_neighbor_id)
+            proxy = xmlrpc.client.ServerProxy(addr)
+            proxy.reply(self.peer_id, new_path)
+
+
+    def reply(self, seller_id, path):
+        footprints = path.split('-')
+        from_neighbor_id = int(footprints[0])
+
+        # buy, TODO what if there are multiple reply?
+        if from_neighbor_id == self.peer_id:
+            addr = "http://localhost:%d/" % (PORT_START_NUM + seller_id)
+            proxy = xmlrpc.client.ServerProxy(addr)
+            # fail
+            result = proxy.buy(self.target, self.target_quantity)
+            if result == -1:
+                pass
+            
+            self.target_quantity -= result
+            
+            # TODO: regenerate a buy request
+            if self.target_quantity == 0:
+                pass
+
+            return True
+
+        new_path = "-".join(footprints[1:])
+        
+        addr = "http://localhost:%d/" % (PORT_START_NUM + from_neighbor_id)
+        proxy = xmlrpc.client.ServerProxy(addr)
+        proxy.reply(self.peer_id, new_path)
 
         return True
 
 
-    def start_peer_server(self):
-        with print_lock:
-            print("The server of peer %d is running" % self.peer_id)
-        server = SimpleXMLRPCServer(("localhost", PORT_START_NUM + self.peer_id))
-        server.register_function(self.show_me_some_message)
-        server.serve_forever()
-
-
-    def lookup(self, product_name, hopcount):
-        pass
-
-    def reply(self, seller_id):
-        pass
-
-    def buy(self, seller_id):
-        pass
+    def buy(self, product_name, quantity):
+        # TODO get the lock first?
+        if product_name != self.commodity:
+            return -1
+        
+        # TODO should generate a new commodity to sell
+        if self.commodity_number <= quantity:
+            return self.commodity_number
+        
+        self.commodity_number -= quantity
+        return quantity
 
 
 if __name__ == "__main__":
